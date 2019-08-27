d12922fb673e12e2178f1424e08a77b65c557366
nick-k-dev <20979916+nick-k-dev@users.noreply.github.com>
2019-08-26 22:05:19 -0400

    Code reformatting completed with breaking up of code into different files

--- 03-racing/Input.js
+++ 03-racing/Input.js
@@ -14,3 +14,34 @@ let keyHeld = {
 };
 
 //END KEYBOARD CONSTANTS AND CHECKS*****************
+
+//********LISTENER CALLBACKS
+const initInput = () => {
+    document.addEventListener('keydown', keyPressed);
+    document.addEventListener('keyup', keyReleased);
+};
+
+const setKeyHoldState = (currentKey, pressedState) => {
+    if(currentKey === KEY.UP_ARROW) {
+        keyHeld.gas = pressedState;
+    }
+    if(currentKey === KEY.DOWN_ARROW){
+        keyHeld.reverse = pressedState;
+    }
+    if(currentKey === KEY.LEFT_ARROW){
+        keyHeld.left = pressedState;
+    }
+    if(currentKey === KEY.RIGHT_ARROW){
+        keyHeld.right = pressedState;
+    }
+}
+
+const keyPressed = (evt) => {
+    setKeyHoldState(evt.keyCode, true);
+    evt.preventDefault();
+}
+
+const keyReleased = (evt) => {
+   setKeyHoldState(evt.keyCode, false);
+}
+//*********END LISTENER CALLBAKCKS
