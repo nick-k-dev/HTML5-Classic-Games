41985fad0c2e673e62d100808c8bdde6e9dcf53f
nick-k-dev <20979916+nick-k-dev@users.noreply.github.com>
2019-08-26 21:38:00 -0400

    Created new files to split up the code

---
+++ 03-racing/Input.js
@@ -0,0 +1,16 @@
+//KEYBORAD CONSTANTS AND CHECKS**********
+const KEY = Object.freeze({
+    LEFT_ARROW: 37,
+    UP_ARROW: 38,
+    RIGHT_ARROW: 39,
+    DOWN_ARROW: 40
+});
+
+let keyHeld = {
+    gas: false,
+    reverse: false,
+    left: false,
+    right: false
+};
+
+//END KEYBOARD CONSTANTS AND CHECKS*****************
